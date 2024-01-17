fce8f672a1db954f23e4bc4a9e3ea8d6270f1747
Brian A <brian.ahlers@gmail.com>
2024-01-17 13:21:46 -0800

    tidy code

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -2,51 +2,39 @@ import React, { useState, useEffect, useRef } from 'react';
 
 
 function Stopwatch() {
-
     const [isRunning, setIsRunning] = useState(false);
     const [elapsedTime, setElapsedTime] = useState(0);
     const intervalIDRef = useRef(null);
     const startTimeRef = useRef(0);
 
     useEffect(() => {
-
         if (isRunning) {
             intervalIDRef.current = setInterval(() => {
                 setElapsedTime(Date.now() - startTimeRef.current);
             }, 10)
-        }
-
+        } 
+        
         return () => {
-
             clearInterval(intervalIDRef.current);
-        }
-
+        };
     }, [isRunning]);
 
     function start() {
-
         setIsRunning(true);
         startTimeRef.current = Date.now() - elapsedTime;
         console.log(startTimeRef.current);
-
     };
 
     function stop() {
-
         setIsRunning(false);
-
     };
 
     function reset() {
-
         setElapsedTime(0);
         setIsRunning(false);
-
-
     };
 
     function formatTime() {
-
         let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
         let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
         let seconds = Math.floor((elapsedTime / 1000) % 60);
