944a58dfb5226bae9264aa73f2beda39fe9b8599
Brian A <brian.ahlers@gmail.com>
2024-01-17 11:24:41 -0800

    set up with vite

---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>React Stopwatch</h1>
+      <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
